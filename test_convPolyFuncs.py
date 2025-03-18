1e78000c61fabba2e47debec2a419a9d01a89c79
Connor Ritchie <connorbr011@gmail.com>
2025-03-18 16:18:04 -0600

    added convPoly unit tests for cases of one and two circles

--- test_convPolyFuncs.py
+++ test_convPolyFuncs.py
@@ -1,7 +1,9 @@
 import convPolyFuncs as cpf
 import packingAlgs as pa
 import drawFuncsV2 as df
-import shapely as sh
+from shapely import Point, Polygon
+from geoThings import Circle, InfLine, VertLine
+from math import isclose, pi
 import unittest
 
 
@@ -21,6 +23,21 @@ def main():
 class Testing(unittest.TestCase):
     def test_sum(self):
         self.assertEqual(3, 2 + 1)
+    
+
+    def test_convPoly_returns_area_of_circle_with_only_one_circle(self):
+        circ = Circle(Point(0,0), 5.0)
+        polyArea = cpf.convPoly([circ])
+        self.assertTrue(isclose(
+            polyArea, 25.0 * pi
+        ))
+
+    def test_convPoly_2_circles_returns_rectangle(self):
+        circs = pa.radSumAlg([5,5])
+        poly = cpf.convPoly(circs)
+        self.assertTrue(isclose(
+            poly.area, 200.0
+        ))
 
 
 if __name__ == "__main__":
